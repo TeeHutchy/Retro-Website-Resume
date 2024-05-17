745279b1449060ad4d21a66f2e3f4595d02a89fa
Taylor Hutchens <minitee@Taylors-Mac-mini.local>
2024-05-16 17:11:00 -0700

    major update for css and footer. generally working

--- src/app/components/macWindow.tsx
+++ src/app/components/macWindow.tsx
@@ -6,6 +6,7 @@ import { GREETING, BIO, DEV1, DEV2, DEV3, DEV4, DEV5, DEV6, DEV7, DEV9, DEVFINAL
 import Modal from './Modal';
 import LinkedInSvg from './LinkedIn';
 import GitHubSvg from './GitHub';
+import EmailIcon from './GmailMe';
 const pdf = '/TaylorDouglasHutchensResume2024.pdf';
 
 interface WindowProps {
@@ -14,9 +15,10 @@ interface WindowProps {
 } 
 
 const PdfIcon = ({ onClick }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-filetype-pdf" viewBox="0 0 16 16" onClick={onClick} style={{ fontSize: '2rem', cursor: 'pointer' }}>
-    <path fillRule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803q.43 0 .732-.173.305-.175.463-.474a1.4 1.4 0 0 0 .161-.677q0-.375-.158-.677a1.2 1.2 0 0 0-.46-.477q-.3-.18-.732-.179m.545 1.333a.8.8 0 0 1-.085.38.57.57 0 0 1-.238.241.8.8 0 0 1-.375.082H.788V12.48h.66q.327 0 .512.181.185.183.185.522m1.217-1.333v3.999h1.46q.602 0 .998-.237a1.45 1.45 0 0 0 .595-.689q.196-.45.196-1.084 0-.63-.196-1.075a1.43 1.43 0 0 0-.589-.68q-.396-.234-1.005-.234zm.791.645h.563q.371 0 .609.152a.9.9 0 0 1 .354.454q.118.302.118.753a2.3 2.3 0 0 1-.068.592 1.1 1.1 0 0 1-.196.422.8.8 0 0 1-.334.252 1.3 1.3 0 0 1-.483.082h-.563zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638z"/>
-  </svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-file-earmark-person" viewBox="0 0 16 16" onClick={onClick} style={{ cursor: 'pointer', padding: '5px' }}>
+  <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5z"/>
+</svg>
 );
 
 const MacWindow: React.FC<{ windowProps: WindowProps }> =  ({ windowProps }) => {
@@ -64,12 +66,11 @@ const MacWindow: React.FC<{ windowProps: WindowProps }> =  ({ windowProps }) => 
                     display: 'inline-block/block',
                     whiteSpace: "pre-line" }} />
 
-
-
                 <div className='footer'>
-                    <PdfIcon onClick={handleOpenModal} />
+                    <PdfIcon onClick={handleOpenModal}/>
                     <LinkedInSvg />
                     <GitHubSvg />
+                    <EmailIcon />
 
           
                     <Modal show={showModal} handleClose={handleCloseModal}>
