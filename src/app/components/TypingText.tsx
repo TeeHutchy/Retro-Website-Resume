989c0020302247d265bea10af41600a5cda5cea4
Taylor Hutchens <minitee@Taylors-Mac-mini.local>
2024-05-17 18:52:53 -0700

    change final text and bio

--- src/app/components/TypingText.tsx
+++ src/app/components/TypingText.tsx
@@ -1,5 +1,5 @@
 import { TypeAnimation } from "react-type-animation"
-import { P1, P2, P3 } from "../constants/string"
+import { DEV, DEV1, DEV2, DEV3, DEV4, DEV6, DEV9, DEVFINAL, LINKS, P1, P2, P3 } from "../constants/string"
 
 
 const TypingBio = () => {
@@ -11,6 +11,15 @@ const TypingBio = () => {
             P1, 0,
             P1 + P2, 0,
             P1 + P2 + P3, 0,
+            P1 + P2 + P3 + DEV, 1000,
+            P1 + P2 + P3 + DEV2, 1000,
+            P1 + P2 + P3 + DEV1, 1000,
+            P1 + P2 + P3 + DEV3, 1000,
+            P1 + P2 + P3 + DEV4, 1000,
+            P1 + P2 + P3 + DEV6, 1000,
+            P1 + P2 + P3 + DEV9, 1000,
+            P1 + P2 + P3 + DEVFINAL, 1000,
+            P1 + P2 + P3 + DEVFINAL + "\n" + LINKS, 1000,
             ]} 
         style={{ 
             fontSize: '1rem',
